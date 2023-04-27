8a43d19d7973fad184ce8daa61a30d976c4f65f4
eva safitri <evaasafitri23@gmail.com>
2023-04-27 21:02:38 +0700

    ini update baru

---
+++ Modul 3 dan 4 _ Intens dan Room Database/modul34/app/src/main/java/com/example/modul34/data/dao/UserDao.kt
@@ -0,0 +1,23 @@
+package com.example.modul34.data.dao
+
+import androidx.room.*
+import com.example.modul34.data.entity.User
+
+
+@Dao
+interface UserDao {
+    @Query("SELECT * FROM user")
+    fun getAll(): List<User>
+
+    @Insert
+    fun insertAll(vararg users: User)
+
+    @Delete
+    fun delete(user: User)
+
+    @Query("SELECT * FROM user WHERE uid = :uid")
+    fun get(uid: Int) : User
+
+    @Update
+    fun update(user: User)
+}
